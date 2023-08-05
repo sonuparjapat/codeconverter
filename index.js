d648d874b4fa2e601818842a1a5c215adae5b50c
sonuparjapat <sahil992001parjapat@gmail.com>
2023-08-05 14:44:32 +0530

    check

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+require("dotenv").config()
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Define API endpoint for code conversion
+app.get("/",async(req,res)=>{
+  res.json({msg:"welcome"})
+})
+app.post('/api/convert', async (req, res) => {
+  const { code, language } = req.body;
+
+  try {
+
+    const apiKey = process.env.OPENAI_API_KEY; // Replace with your actual OpenAI API key
+    // console.log(apiKey)
+    // console.log(code,language)
+    const prompt = `Translate the following code from ${language} to ${language === 'javascript' ? 'python' : 'javascript'}:\n${code}\n\n`;
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        prompt,
+        max_tokens: 200,
+        n: 1,
+        stop: '\n',
+        temperature: 0.7,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }
+    );
+
+    // Process the response and extract the converted code from the GPT API output
+    const convertedCode =await response.data.choices[0].text;
+console.log(response.data.choices[0])
+    res.json({ convertedCode });
+  } catch (error) {
+    console.error('Error converting code:', error.message);
+    res.status(500).json({ error: 'Code conversion failed' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
