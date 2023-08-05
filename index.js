f3b21467365d3e810e7f774eb1ac6828e4024d9a
sonuparjapat <sahil992001parjapat@gmail.com>
2023-08-05 14:50:49 +0530

    chea

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
+const cors=require('cors')
 require("dotenv").config()
-const port = process.env.PORT || 5000;
+const app = express();
 
+const port = process.env.PORT || 5000;
+app.use(cors())
 app.use(express.json());
 
 // Define API endpoint for code conversion
